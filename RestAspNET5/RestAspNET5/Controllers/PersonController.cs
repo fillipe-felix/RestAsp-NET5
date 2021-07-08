9d875ad4cf218727b91034128248c6f6da66a75a
fillipe-felix <felipesoares_1993@hotmail.com>
2021-07-08 13:38:11 -0300

    inserting "api/" path url and update .gitignore

--- RestAspNET5/RestAspNET5/Controllers/PersonController.cs
+++ RestAspNET5/RestAspNET5/Controllers/PersonController.cs
@@ -6,7 +6,7 @@ using RestAspNET5.Services;
 namespace RestAspNET5.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
