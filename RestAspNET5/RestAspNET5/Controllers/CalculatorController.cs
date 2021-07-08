15513bca6d85e5882e017b8c72ca3f277fc7a79a
fillipe-felix <felipesoares_1993@hotmail.com>
2021-07-08 10:22:00 -0300

    finalized calculator

--- RestAspNET5/RestAspNET5/Controllers/CalculatorController.cs
+++ RestAspNET5/RestAspNET5/Controllers/CalculatorController.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -16,12 +17,72 @@ namespace RestAspNET5.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (isNumeric(firstNumber) && isNumeric(secondNumber))
             {
                 var sum = ConevertToDecimal(firstNumber) + ConevertToDecimal(secondNumber);
                 return Ok(sum.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+        
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var subtraction = ConevertToDecimal(firstNumber) - ConevertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var multiplication = ConevertToDecimal(firstNumber) * ConevertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+        
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConevertToDecimal(firstNumber) / ConevertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+        
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var mean = (ConevertToDecimal(firstNumber) + ConevertToDecimal(secondNumber)) / 2;
+                return Ok(mean.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+        
+        [HttpGet("square-root/{firstNumber}")]
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (isNumeric(firstNumber))
+            {
+                var squareRoot = Math.Sqrt((double)ConevertToDecimal(firstNumber));
+                return Ok(squareRoot.ToString());
             }
             
             return BadRequest("Invalid input");
