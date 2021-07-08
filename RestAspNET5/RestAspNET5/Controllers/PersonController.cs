6ea665c5024dca035a3e5b2a8b3edb37fa734a21
fillipe-felix <felipesoares_1993@hotmail.com>
2021-07-08 10:31:58 -0300

    working with four verbs

---
+++ RestAspNET5/RestAspNET5/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestAspNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+        
+        
+    }
+}
