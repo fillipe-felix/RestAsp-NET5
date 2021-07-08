cdf0c036318863425601be8272ef8e72cdc2d392
fillipe-felix <felipesoares_1993@hotmail.com>
2021-07-08 09:40:41 -0300

    implemeting calculator

---
+++ RestAspNET5/RestAspNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestAspNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConevertToDecimal(firstNumber) + ConevertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            
+            return BadRequest("Invalid input");
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+        
+        private decimal ConevertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        
+    }
+}
